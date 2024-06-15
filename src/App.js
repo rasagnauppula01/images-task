2db1b0d0ef3763c6ab4289ef16a72e79084ec971
rasagnauppula01 <rasagnauppula10@gmail.com>
2024-06-15 18:35:10 +0530

    new-add

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
-import logo from './assets/images/Vector.svg';
-import logo1 from "./assets/images/img_group_101 (1).png"
- 
+// import logo from './assets/images/Vector.svg';
+// import logo1 from "./assets/images/img_group_101 (1).png"
+ import React from 'react';
+import Header from './header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo1} className="App-logo" alt="logo" />
-        <span>
-          lorem 
-        </span>
-        
-      </header>
+      <Header/>
     </div>
   );
 }
---
+++ src/header.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Header.css';
+import logo2 from "./assets/images/img_group_101 (1).png"
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="logo">
+        <img src={logo2} alt="logo1" width={50} height={40} ></img>
+        <span className='inspiration-regular'>Ik developers</span>
+      </div>
+      <nav>
+        <ul>
+          <li>About us</li>
+          <li>Services</li>
+          <li>Case Studies</li>
+          <li>Blog</li>
+          <li>How it Works</li>
+          <li>Hire</li>
+        </ul>
+      </nav>
+      <button>Contact us</button>
+    </header>
+  );
+};
+
+export default Header;
