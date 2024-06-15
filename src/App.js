b3ff39602f910822a0dc689497d24296a74445ba
rasagnauppula01 <rasagnauppula10@gmail.com>
2024-06-15 18:29:54 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './assets/images/Vector.svg';
-
+import logo1 from "./assets/images/img_group_101 (1).png"
+ 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo1} className="App-logo" alt="logo" />
         <span>
           lorem 
         </span>
